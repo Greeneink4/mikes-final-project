3f46a7ca9e75961e4c8f25f17f6834054c36ae80
Michael Greene <greeneink4@gmail.com>
2017-11-15 20:53:57 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import EasyTrails from './easy-trails';
-import ModerateTrails from './moderate-trails';
-import DifficultTrails from './difficult-trails';
 import NavigationBar from './navigation-bar';
 import './App.css';
 
@@ -10,25 +6,20 @@ import './App.css';
 export default class App extends Component {
     render() {
         return (
-            <MuiThemeProvider>
-                <div className="navigation-bar">
-                    <NavigationBar/>
-                </div>
-                    {/*<div className="header">*/}
-                        {/*<h1>Uintah Basin Trails Project</h1>*/}
-                    {/*</div>*/}
-                    <div className="trails-contain">
-                        <div><EasyTrails/></div>
-                        <div><ModerateTrails/></div>
-                        <div><DifficultTrails/></div>
-                    </div>
-                <div className="footer">
-                    <h2>This is my footer.</h2>
-                </div>
-            </MuiThemeProvider>
+            <div className="navigation-menu">
+                <NavigationBar/>
+            </div>
+
+
+                //     <div className="trails-contain">
+                //         <div><EasyTrails/></div>
+                //         <div><ModerateTrails/></div>
+                //         <div><DifficultTrails/></div>
+                //     </div>
+                // <div className="footer">
+                //     <h2>This is my footer.</h2>
+                // </div>
         );
-     }
     }
-
-
+}
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import EasyTrails from './easy-trails';
+import ModerateTrails from './moderate-trails';
+import DifficultTrails from './difficult-trails';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import './index.css';
 import App from './App';
@@ -8,5 +13,20 @@ import App from './App';
 
 injectTapEventPlugin();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <MuiThemeProvider>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route path="/easy" component={EasyTrails}/>
+                <Route path="/moderate" component={ModerateTrails}/>
+                <Route path="/difficult" component={DifficultTrails}/>
+            </Switch>
+        </BrowserRouter>
+        <EasyTrails/>
+        <ModerateTrails/>
+        <DifficultTrails/>
+    </MuiThemeProvider>
+
+), document.getElementById('root'));
 //registerServiceWorker();
