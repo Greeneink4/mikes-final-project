ce007dc426629199d6e8403f23a6ec40660c96c6
Michael Greene <greeneink4@gmail.com>
2017-11-26 17:42:11 -0700

    Apollo and GraphQL changes

--- src/index.js
+++ src/index.js
@@ -2,20 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client'; 
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+// import { createNetworkInterface } from 'react-apollo';
 import EasyTrails from './easy-trails';
 import ModerateTrails from './moderate-trails';
 import DifficultTrails from './difficult-trails';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import TrailDetails from './trail-details';
 import './index.css';
 import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 
+// const networkInterface = createNetworkInterface({
+//     uri: process.env.REACT_APP_GRAPHQL_URI
+// });
 
-injectTapEventPlugin();
+const client = new ApolloClient({
+    link: new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjahc39by157a0179fy5qmjwy' }),
+    cache: new InMemoryCache()
+    });
+    // cache: new InMemoryCache(),
+    // connectToDevTools: process.env.NODE_ENV === 'development',
+    // dataIdFromObject: o => o.id
+// });
 
-ReactDOM.render((
+
+
+ReactDOM.render(
     <MuiThemeProvider>
+        <ApolloProvider client={client}>
+            {/* <App /> */}    
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={App}/>
@@ -25,7 +43,21 @@ ReactDOM.render((
                 <Route path="/trail-details" component={TrailDetails}/>
             </Switch>
         </BrowserRouter>
-    </MuiThemeProvider>
+        </ApolloProvider>  
+    </MuiThemeProvider>,
+document.getElementById('root')
+)
 
-), document.getElementById('root'));
+
+
+
+
+
+
+// ReactDOM.render(
+    // <ApolloProvider client={client}>
+    //    <App />
+    // </ApolloProvider>,    
+    
+
 //registerServiceWorker();
--- src/services/get-trails.js
+++ src/services/get-trails.js
@@ -1,30 +1,30 @@
 
-import React from 'react';
-import axios from 'axios';
+// import React from 'react';
+// import axios from 'axios';
 
 
-export default function getTrailByLatLon(lat, lon) {
-    axios.get('https://www.hikingproject.com/data/get-trails', {
-        params: {
-            key: "200180338 - 4bfdf77c160c65d3febdb97a06ebd6c9",
-            lat,
-            lon,
-            maxDistance: 10,
-            maxResults: 10
-        },
-        headers: {
+// export default function getTrailByLatLon(lat, lon) {
+//     axios.get('https://www.hikingproject.com/data/get-trails', {
+//         params: {
+//             key: "200180338 - 4bfdf77c160c65d3febdb97a06ebd6c9",
+//             lat,
+//             lon,
+//             maxDistance: 10,
+//             maxResults: 10
+//         },
+//         headers: {
 
-            // 'Content-type': 'application/json'
-        }
-    })
-
-
-    // 'Content-type': 'application/jason'
+//             // 'Content-type': 'application/json'
+//         }
+//     })
 
 
-}
+//     // 'Content-type': 'application/jason'
 
-//         .then(function (response) {
+
+// }
+
+// //         .then(function (response) {
 //             return response
 //         })
 //         .catch(function (error) {
